4e5c63105552ca55dc2d39c0eb94c9ba9026563e
Chris Luo <chris.ying.luo@gmail.com>
2017-11-12 20:12:21 -0800

    ng: Clean up subscription.

--- ng-chat/src/app/chat/chat.component.ts
+++ ng-chat/src/app/chat/chat.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { ChatService } from './chat.service';
+import { Subject } from 'rxjs/Subject';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
@@ -9,16 +11,24 @@ import { ChatService } from './chat.service';
   providers: [ChatService],
   encapsulation: ViewEncapsulation.None
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   messages = [];
+  private ngUnsubscribe: Subject<any> = new Subject();
 
   constructor(private chatService: ChatService) { }
 
   ngOnInit() {
-    this.chatService.newMessage.subscribe(data => {
+    this.chatService.newMessage
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe(data => {
       console.log(data);
       this.messages.push(data);
     });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 
   onSendMessage(f: NgForm) {
