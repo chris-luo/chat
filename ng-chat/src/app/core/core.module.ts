dcb25686b0b88c0d6148d9b5f5743d32eb20d9f2
Chris Luo <chris.ying.luo@gmail.com>
2017-11-12 19:03:41 -0800

    ng: Added home component.

--- ng-chat/src/app/core/core.module.ts
+++ ng-chat/src/app/core/core.module.ts
@@ -4,10 +4,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from '../shared/material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
     declarations: [
-        NavbarComponent
+        NavbarComponent,
+        HomeComponent
     ],
     imports: [
         FormsModule,
