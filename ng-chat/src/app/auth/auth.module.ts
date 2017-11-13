58beafb0a93f04ea1bd1e5adbc946f45550d4dc9
Chris Luo <chris.ying.luo@gmail.com>
2017-11-12 22:31:06 -0800

    ng: Added signup component conent.

--- ng-chat/src/app/auth/auth.module.ts
+++ ng-chat/src/app/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { AuthRoutingModule } from "./auth-routing.module";
 import { SigninComponent } from "./signin/signin.component";
 import { SignupComponent } from "./signup/signup.component";
+import { MaterialModule } from "../shared/material.module";
 
 @NgModule({
     declarations: [
@@ -9,7 +12,9 @@ import { SignupComponent } from "./signup/signup.component";
         SignupComponent
     ],
     imports: [
-        AuthRoutingModule
+        AuthRoutingModule,
+        ReactiveFormsModule,
+        MaterialModule
     ]
 })
 
--- ng-chat/src/app/auth/signup/signup.component.ts
+++ ng-chat/src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,10 +8,23 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initalizeForm();
+  }
+
+  initalizeForm() {
+    this.signupForm = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)]),
+      "password": new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
+    })
+  }
+
+  onSignUp() {
+    console.log(this.signupForm);
   }
 
 }
