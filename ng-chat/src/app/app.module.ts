68998aa16adf667a0b86a1b9f08afddb5ab9c9c1
Chris Luo <chris.ying.luo@gmail.com>
2017-11-12 21:34:45 -0800

    ng: Added auth module.

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ng-chat/src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { AuthRoutingModule } from "./auth-routing.module";
+import { SigninComponent } from "./signin/signin.component";
+import { SignupComponent } from "./signup/signup.component";
+
+@NgModule({
+    declarations: [
+        SigninComponent,
+        SignupComponent
+    ],
+    imports: [
+        AuthRoutingModule
+    ]
+})
+
+export class AuthModule {
+
+}
