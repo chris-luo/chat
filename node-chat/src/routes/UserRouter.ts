e0736b415c2118984700d25dda14a5ed10387894
Chris Luo <chris.ying.luo@gmail.com>
2017-11-14 23:45:12 -0800

    node: Connect signup api to database, add username validation.

--- node-chat/src/routes/UserRouter.ts
+++ node-chat/src/routes/UserRouter.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { check, validationResult } from "express-validator/check";
 import { matchedData, sanitizeBody } from "express-validator/filter";
+import db from "../controllers/Database";
+import { QueryConfig } from "pg";
+
 export class UserRouter {
     router: Router;
     constructor() {
@@ -10,17 +13,34 @@ export class UserRouter {
 
     private signup(req: Request, res: Response, next: NextFunction) {
         const errors = validationResult(req);
-        console.log(req.body)
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.mapped() });
         }
         const user = matchedData(req);
-        console.log(user);
-        res.json(user);
+        let queryConfig: QueryConfig = {
+            text: `INSERT INTO chat_user(username, email, password) VALUES($1, $2, $3) RETURNING *`,
+            values: [user.username, user.email, user.password]
+        }
+        db.query(queryConfig, (error, results) => {
+            if (error) {
+                console.log(error);
+
+                return res.status(400).json({
+                    statusText: error
+                });
+            }
+            else {
+                console.log(results);
+                res.json({
+                    statusText: `You may now log in.`
+                });
+            }
+        });
     }
 
     private signupValidation() {
         return [
+            check('username').exists(),
             check('email').isEmail().trim().normalizeEmail(),
             check('password').matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
         ]
