2a3d31a2bec925a17cd4f50aff3c89ffdf4d9c9c
Chris Luo <chris.ying.luo@gmail.com>
2017-11-16 20:54:48 -0800

    ng: Added signin api.

--- ng-chat/src/app/auth/auth.service.ts
+++ ng-chat/src/app/auth/auth.service.ts
@@ -3,10 +3,15 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
+    private ep: string = "http://localhost:3000/users";
 
     constructor(private http: HttpClient) {}
 
     signup(user: {username: string, email: string, password: string}) {
-        return this.http.post('http://localhost:3000/users/signup', user)
+        return this.http.post(`${this.ep}/signup`, user);
+    }
+
+    signin(user: {email: string, password: string}) {
+        return this.http.post(`${this.ep}/signin`, user);
     }
 }
