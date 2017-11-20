d0001e2cd441ba3345003b5741e30ce0bc40f508
Chris Luo <chris.ying.luo@gmail.com>
2017-11-19 16:33:42 -0800

    ng: Registered chat reducers.

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
+import { StoreModule } from '@ngrx/store/src/store_module';
+import { chatReducer } from './chat/store/chat.reducers';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     BrowserModule,
     CoreModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot({chat: chatReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
