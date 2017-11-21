c486fafc9840ece0cfad6e095d300afaee8e0612
Chris Luo <chris.ying.luo@gmail.com>
2017-11-20 22:20:02 -0800

    ng: Clean up code.

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store/src/store_module';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store/src/store_module';
-import { chatReducer } from './chat/store/chat.reducers';
 
 @NgModule({
   declarations: [
