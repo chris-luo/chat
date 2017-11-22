290e1db7fc5fe8809688ce56d33c53247bc53b2c
Chris Luo <chris.ying.luo@gmail.com>
2017-11-21 22:53:44 -0800

    ng: Added EffectsModule.

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store/src/store_module';
+import { EffectsModule } from '@ngrx/effects/src/effects_module';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 
 import { reducers } from './store/app.reducers';
+import { AuthEffects } from './auth/store/auth.effects';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { reducers } from './store/app.reducers';
     BrowserModule,
     CoreModule,
     AuthModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
