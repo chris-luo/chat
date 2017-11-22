3b0cbfadabcf44f4d72b307ca9f996ee41eab653
Chris Luo <chris.ying.luo@gmail.com>
2017-11-22 00:18:19 -0800

    ng: Logout dispatch action.

--- ng-chat/src/app/core/navbar/navbar.component.ts
+++ ng-chat/src/app/core/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
-import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+
+import { AuthService } from '../../auth/auth.service';
 import * as fromApp from '../../store/app.reducers';
 import { Observable } from 'rxjs/Observable';
 import * as fromAuth from '../../auth/store/auth.reducers';
+import * as AuthActions from '../../auth/store/auth.actions';
+
 
 @Component({
   selector: 'app-navbar',
@@ -15,14 +19,16 @@ import * as fromAuth from '../../auth/store/auth.reducers';
 export class NavbarComponent implements OnInit {
   authState$: Observable<fromAuth.State>;
 
-  constructor(public authService: AuthService, private store: Store<fromApp.AppState>) { }
+  constructor(public authService: AuthService, private store: Store<fromApp.AppState>, private router: Router) { }
 
   ngOnInit() {
     this.authState$ = this.store.select('auth');
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
+    this.router.navigate(['/signin']);
+    // this.authService.logout();
   }
 
 }
