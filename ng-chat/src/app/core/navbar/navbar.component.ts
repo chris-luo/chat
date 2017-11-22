6c66cb01c03bd64a1abdc73f222cdcf2e974f160
Chris Luo <chris.ying.luo@gmail.com>
2017-11-21 21:43:02 -0800

    ng: Use auth store for navbar.

--- ng-chat/src/app/core/navbar/navbar.component.ts
+++ ng-chat/src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store/src/store';
+import * as fromApp from '../../store/app.reducers';
+import { Observable } from 'rxjs/Observable';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +13,12 @@ import { AuthService } from '../../auth/auth.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent implements OnInit {
+  authState$: Observable<fromAuth.State>;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState$ = this.store.select('auth');
   }
 
   onLogout() {
