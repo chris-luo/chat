2f4c21a05b2faa571309054d6c89a2bf25be9797
Chris Luo <chris.ying.luo@gmail.com>
2017-11-21 22:54:06 -0800

    ng: Added auth effects.

---
+++ ng-chat/src/app/auth/store/auth.effects.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Effect } from '@ngrx/effects';
+import { Actions } from '@ngrx/effects/src/actions';
+import * as AuthActions from './auth.actions';
+import { map, mergeMap } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
+// import 'rxjs/add/operator/map';
+@Injectable()
+export class AuthEffects {
+    @Effect()
+    authSignup = this.actions$
+        .ofType(AuthActions.TRY_SIGNUP)
+        .pipe(
+            map((action: AuthActions.TrySignup) => {
+                return action.payload;
+            }),
+            map((user: {username: string, email: string, password: string}) => {
+                return this.authService.signup(user)
+            }),
+            mergeMap((res) => {
+                console.log(res);
+                return [
+                    {
+                        type: AuthActions.SIGNUP
+                    },
+                    {
+                        type: AuthActions.SET_TOKEN,
+                        payload: res['data']
+                    }
+                ]
+            })
+        );
+
+    constructor(private actions$: Actions, private authService: AuthService) {}
+}
