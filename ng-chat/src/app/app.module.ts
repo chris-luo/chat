afbeb658a904a36deefe80dbcd748431f29ffeda
Chris Luo <chris.ying.luo@gmail.com>
2017-11-22 22:22:28 -0800

    ng: Added store devtools package.

--- ng-chat/src/app/app.module.ts
+++ ng-chat/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -19,7 +21,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     CoreModule,
     AuthModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
