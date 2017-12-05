34912542210de37cb1495a74b353d149fed7e000
Chris Luo <chris.ying.luo@gmail.com>
2017-12-04 21:28:24 -0800

    ng: Added users to chat model.

--- ng-chat/src/app/chat/chat.model.ts
+++ ng-chat/src/app/chat/chat.model.ts
@@ -2,5 +2,5 @@ import { Message } from './message.model';
 import { User } from '../shared/user.model';
 
 export class Chat {
-    constructor(public id: number, public messages: Message[]) {}
+    constructor(public id: number, public users: User[], public messages: Message[]) {}
 }
