8712eb6667ba7e7d2f93a38600169912d9576b92
Chris Luo <chris.ying.luo@gmail.com>
2017-12-05 22:13:06 -0800

    ng: Added api service with find user.

---
+++ ng-chat/src/app/shared/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class ApiService {
+    private apiEndPoint: string = environment.apiEndPoint;
+    constructor(private http: HttpClient) {}
+
+    findUser(user: string) {
+        const params = new HttpParams().set('user', user);
+        return this.http.get(`${this.apiEndPoint}/user`, {params: params});
+    }
+}
