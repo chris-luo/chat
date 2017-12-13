7c55b1315d37e134432654345c10781fec0520eb
Chris Luo <chris.ying.luo@gmail.com>
2017-12-12 23:31:30 -0800

    ng: Changed id to string.

--- ng-chat/src/app/chat/chat.model.ts
+++ ng-chat/src/app/chat/chat.model.ts
@@ -2,5 +2,5 @@ import { Message } from './message.model';
 import { User } from '../shared/user.model';
 
 export class Chat {
-    constructor(public id: number, public users: User[], public messages: Message[]) {}
+    constructor(public id: string, public users: User[], public messages: Message[]) {}
 }
