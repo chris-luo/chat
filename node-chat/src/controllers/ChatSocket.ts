c3e7cc1afc5dd80746845c12704797bc9f8308ed
Chris Luo <chris.ying.luo@gmail.com>
2017-12-16 00:51:40 -0800

    node: Insert message in socket post.

--- node-chat/src/controllers/ChatSocket.ts
+++ node-chat/src/controllers/ChatSocket.ts
@@ -1,4 +1,6 @@
 import * as socketIo from "socket.io";
+import db from "../controllers/Database";
+import { QueryConfig } from "pg";
 
 export class ChatSocket {
     private io: SocketIO.Server;
@@ -11,11 +13,39 @@ export class ChatSocket {
         this.io.on('connect', (socket: SocketIO.Socket) => {
             console.log('a user connected');
             socket.on('join', (room: string) => {
+                console.log(room);
                 socket.join(room);
             });
             socket.on('post', (data:any) => {
                 console.log(data);
-                socket.broadcast.emit('message', data);
+                (async() => {
+                    const client = await db.getClient();
+                    try {
+                        await client.query('BEGIN');
+                        const queryConfig: QueryConfig = {
+                            text: `INSERT INTO chat(id, total_messages) VALUES($1, 1) ON CONFLICT (id) DO UPDATE SET total_messages = chat.total_messages + 1 RETURNING *`,
+                            values: [data[1].id]
+                        }
+                        const rows = await client.query(queryConfig);
+                        console.log(rows.rows);
+                        const queryConfig2: QueryConfig = {
+                            text: `INSERT INTO message(id, content, sender) VALUES($1, $2, $3) RETURNING *`,
+                            values: [`${rows.rows[0].id}:${rows.rows[0].total_messages}`, data[0].content, data[0].sender]
+                        };
+                        const rows2 = await client.query(queryConfig2);
+                        console.log(rows2.rows)
+                        await client.query('COMMIT');
+                        const message = rows2.rows[0];
+                        this.io.in(rows.rows[0].id).emit('message', message);
+                    } catch (error) {
+                        await client.query('ROLLBACK');
+                        //TODO: Emit error here
+                    } finally {
+                        client.release();
+                    }
+                })().catch(error => {
+                    //TODO: Emit error here
+                });
             });
             socket.on('disconnect', () => {
                 console.log('disconnected');
