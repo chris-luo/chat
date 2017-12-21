7f3a8a45644a03bcfb88d3b78a71efe0329eb8b4
Chris Luo <chris.ying.luo@gmail.com>
2017-12-20 22:05:05 -0800

    ng: Clean up chat service.

--- ng-chat/src/app/chat/chat.service.ts
+++ ng-chat/src/app/chat/chat.service.ts
@@ -1,25 +1,18 @@
 import { OnInit, Injectable } from "@angular/core";
-import * as io from 'socket.io-client';
-import { AuthService } from "../auth/auth.service";
 import * as ChatActions from './store/chat.actions';
 import { Store } from "@ngrx/store";
 import { Message } from "./message.model";
 import * as fromChat from './store/chat.reducers';
-import { environment } from '../../environments/environment';
 import * as fromAuth from '../auth/store/auth.reducers';
 import { take } from "rxjs/operators";
 import * as format from 'date-fns/format';
 
 @Injectable()
 export class ChatService {
-    private url = environment.apiEndPoint;
-    private socket;
-
     private user: {id: number, username: string, email: string};
-    private currentChat
+    private currentChat;
 
-    constructor(private authService: AuthService, private store: Store<fromChat.FeatureState>) {
-        // this.connect();
+    constructor(private store: Store<fromChat.FeatureState>) {
         this.store.select('auth')
             .pipe(
                 take(1)
@@ -33,21 +26,6 @@ export class ChatService {
             .subscribe((chatState: fromChat.State) => {
                 this.currentChat = chatState.chats[chatState.currentChat];
             });
-    }
-
-    private connect() {
-        this.socket = io(this.url);
-        this.socket.on('message', (data: Message) => {
-            this.messageDispatcher(data);
-        });
-    }
-
-    private messageDispatcher(data: Message) {
-        this.store.dispatch(new ChatActions.AddMessage(data));
-    }
-
-    disconnect() {
-        this.socket.disconnect();
     }
 
     sendMessage(message: string) {
