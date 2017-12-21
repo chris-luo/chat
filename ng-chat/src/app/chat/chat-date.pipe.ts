6edf2a49a2d9dd743cbade3d5bf13f1ee97b771c
Chris Luo <chris.ying.luo@gmail.com>
2017-12-20 21:54:27 -0800

    ng: Changed date format.

--- ng-chat/src/app/chat/chat-date.pipe.ts
+++ ng-chat/src/app/chat/chat-date.pipe.ts
@@ -1,23 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as format from 'date-fns/format';
 import * as differenceInDays from 'date-fns/difference_in_days';
+import * as differenceInHours from 'date-fns/difference_in_hours'
 @Pipe({
     name: 'chatDate'
 })
 
 export class ChatDatePipe implements PipeTransform {
     transform(date: any) {
-        let days = differenceInDays(new Date(), date);
-        switch (days) {
-            case 0:
-                return format(date, 'h:mm A');
-            case 1:
-                return 'Yesterday';
-            case 2:
-            case 3:
-                return format(date, 'dddd');
-            default:
-                return format(date, 'M/D/YY');
+        let hours = differenceInHours(new Date(), date);
+        console.log(hours);
+        if (hours < 12) {
+            return format(date, 'h:mm A');
         }
+        if (hours < 24) {
+            return 'Yesterday';
+        }
+        if (hours < 72) {
+            return format(date, 'dddd');
+        }
+        return format(date, 'M/D/YY');
     }
 }
