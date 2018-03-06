659082c3c8ce7e646f00388cac593ec983b07779
Chris Luo <chris.ying.luo@gmail.com>
2018-03-06 00:09:49 -0800

    ng-chat: Added id to get chats api.

--- ng-chat/src/app/shared/api.service.ts
+++ ng-chat/src/app/shared/api.service.ts
@@ -16,7 +16,7 @@ export class ApiService {
         return this.http.post(`${this.apiEndPoint}/users/chat`, chat);
     }
 
-    getChats() {
-        return this.http.get(`${this.apiEndPoint}/users/chats`);
+    getChats(id: number) {
+        return this.http.get(`${this.apiEndPoint}/users/${id}/chats`);
     }
 }
